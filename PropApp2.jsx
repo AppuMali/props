2363c11a6e813e323bb3dfb02a73639c9ab36816
AppuMali <126770741+AppuMali@users.noreply.github.com>
2023-03-08 14:41:09 -0500

    Add files via upload

---
+++ PropApp2.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import PropNav2 from './PropComponents/PropNav2'
+
+const PropApp2 = () => {
+  return (
+    <PropNav2 data = {{username:"Appu",password:1234}} />
+  )
+}
+
+export default PropApp2
---
+++ PropApp3.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import PropNav3 from './PropComponents/PropNav3'
+
+const PropApp3 = () => {
+  return (
+    <PropNav3 string = "hello" number = {143} boolean={true} data={null}  />
+  )
+}
+
+export default PropApp3
